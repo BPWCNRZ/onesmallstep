4cb59c6e47daf46b4f7e7f43877dbbc8930d62af
BPWCNRZ <96707000+BPWCNRZ@users.noreply.github.com>
2025-01-10 11:19:39 +0000

    Create Project1.cpp

---
+++ Project1.cpp
@@ -0,0 +1,14 @@
+///Creating a calulcaor to get the length of the hypotenuse of a right triangle
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    //a squared + b squared = c squared
+
+    double a;
+    double b;
+    double c;
+
+    
+}
