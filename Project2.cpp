9537289a248ec9d71f2f18607b055891e222ff19
BPWCNRZ <96707000+BPWCNRZ@users.noreply.github.com>
2025-04-11 22:35:24 +0100

    changes

--- Project2.cpp
+++ Project2.cpp
@@ -1,3 +1,14 @@
 //General purpose calculator 
 
 #include <iostream>
+
+int main() {
+
+    int  number;
+    int guess;
+    int tries;
+
+    std::cout<<"WElcome to the game lets play!";
+    std::cin>>number;
+    
+}
---
+++ Project3.cpp
@@ -0,0 +1,23 @@
+//Number guessing game
+
+#include <iostream>
+
+int main() {
+
+    int number;
+    int guess;
+    int tries;
+
+    srand(time(0));
+    number = rand() % 100 +1;
+
+    
+
+
+    std::cout<<"Welcome to the number guessing game ";
+
+
+
+
+
+}
